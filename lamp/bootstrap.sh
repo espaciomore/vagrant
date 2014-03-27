f46db4a36ec3a189387b1905e89dd55143eeeaf7
espaciomore <espaciomore@gmail.com>
2014-03-27 15:14:43 -0400

    Update bootstrap.sh

--- lamp/bootstrap.sh
+++ lamp/bootstrap.sh
@@ -6,6 +6,7 @@ apt-get install -y mysql-client
 apt-get -f install -y mysql-server-5.5
 apt-get install -y php5-cli
 apt-get install -y libapache2-mod-php5
+#sudo a2enmod rewrite
 apt-get install -y php5-mysql
 apt-get install -y git
 apt-get install -y vim
